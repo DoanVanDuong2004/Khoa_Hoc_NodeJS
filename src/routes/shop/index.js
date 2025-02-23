845909dc92d0e9488c7f1d2a120b2cee1211c8cb
duongdz123 <doanvanduong1472021@gmail.com>
2025-02-23 21:45:41 +0700

    Cập nhật code bai 6

---
+++ src/routes/shop/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const { apiKey,permissions } = require('../../auth/checkAuth')
+const router = express.Router()
+
+//check apikey
+router.use(apiKey)
+router.use(permissions('0000'))
+
+//check permisstion
+
+router.use('/v1/api', require('../access'))
+module.exports = router
